fe0963ae17d76f7ef5756c3e86a16b49ab387483
fahd alghtni <fahdalghtni246@gmail.com>
2024-09-12 13:32:18 +0300

    W3-D5-lab3

---
+++ min.js
@@ -0,0 +1,37 @@
+let one =document.getElementById('one')
+let two =document.getElementById("two")
+let three =document.getElementById("three")
+let four =document.getElementById("four")
+let five =document.getElementById("five")
+let six =document.getElementById("six")
+let seven =document.getElementById("seven")
+let eight =document.getElementById("eight")
+let nine =document.getElementById("nine")
+let img1=document.getElementById("img1")
+one.addEventListener('click', function() {
+    img1.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+
+});
+two.addEventListener('click', function() {
+    img1.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+
+
+});
+three.addEventListener('click', function() {
+    img1.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+
+
+
+});
+four.addEventListener('click', function() {
+    img1.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+
+
+
+});
+five.addEventListener('click', function() {
+    img1.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+
+
+
+});
